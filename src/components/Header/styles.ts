7e01165261d177221244dad1f422edc43785f9bd
yuri neves <yurineves1934@gmail.com>
2023-06-17 13:59:08 -0300

    feat: create model project ts-react

--- src/components/Header/styles.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from 'styled-components';
-
-export const TagHeader = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 70px 120px;
-  position: relative;
-`;
